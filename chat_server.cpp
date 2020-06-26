520bdb514b13aa5b37ff6f76c15633701a2bda85
ugur <ugurbd@gmail.com>
2020-06-26 21:36:09 +0300

    little changes

--- chat_server.cpp
+++ chat_server.cpp
@@ -79,9 +79,9 @@ SSL_CTX *create_context(){
 
     ctx = SSL_CTX_new(method);
     if (!ctx) {
-	perror("Unable to create SSL context");
-	ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
+		perror("Unable to create SSL context");
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
     }
 
     return ctx;
@@ -93,12 +93,12 @@ void configure_context(SSL_CTX *ctx){
     /* Set the key and cert */
     if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
     }
 
     if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
         ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
     }
 }
 
